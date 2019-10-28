9c7b2742a32501bbc0a0f0f782a864c5ecf5b1f3
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2019-10-27 20:37:07 -0600

    test

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 
@@ -22,43 +22,13 @@ app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Headers',
   'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods',
-  'GET, POST, PATCH, DELETE, OPTIONS');
+  'GET, POST, PATCH, PUT, DELETE, OPTIONS');
 
   next();
 });
 
-app.post("/api/posts",(req,res,next)=>{
+//filtering only when path starts with this
+app.use('/api/posts',postsRoutes)
 
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  post.save().then(createdPost =>{
-    res.status(201).json({
-      message:'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find()
-    .then(documents =>{
-      res.status(200).json({
-        message:'Posts fetched successfully',
-        posts:documents
-      });
-    });
-
-});
-
-app.delete("/api/posts/:id",(req,res,next)=>{
-  Post.deleteOne({_id: req.params.id}).then(result=>{
-    console.log(result);
-    res.status(200).json({message: "Post deleted"});
-  });
-
-})
 
 module.exports = app;
