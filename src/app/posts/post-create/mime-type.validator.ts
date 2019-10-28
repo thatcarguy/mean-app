8085ac5d38dfd6fd515c9a62bc66a00c28b95637
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2019-10-27 22:38:20 -0600

    Image upload Angular Changes

---
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -0,0 +1,43 @@
+import { AbstractControl } from "@angular/forms";
+import { Observable, Observer } from "rxjs";
+
+// Syntax we are saying that we expect our return type to have a key that is type string is all.
+export const mimeType = (control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key:string]:any}> => {
+  const file = control.value as File;
+  const fileReader = new FileReader();
+  // Creating observable
+  const frObs = Observable.create((observer: Observer<{[key:string]:any}>)=>{
+    fileReader.addEventListener("loadend", () => {
+      // This is good because it prevents people from changing the file extension
+      const arr = new Uint8Array(fileReader.result as ArrayBuffer).subarray(0, 4);
+      let header = "";
+      let isValid = false;
+      for(let i=0; i<arr.length; i++){
+        header+=arr[i].toString(16);
+      }
+      switch (header) {
+        case "89504e47":
+          isValid = true;
+          break;
+        case "ffd8ffe0":
+        case "ffd8ffe1":
+        case "ffd8ffe2":
+        case "ffd8ffe3":
+        case "ffd8ffe8":
+          isValid = true;
+          break;
+        default:
+          isValid = false; // Or you can use the blob.type as fallback
+          break;
+      }
+      if (isValid) {
+        observer.next(null);
+      } else {
+        observer.next({ invalidMimeType: true });
+      }
+      observer.complete();
+    });
+    fileReader.readAsArrayBuffer(file);
+  });
+  return frObs;
+};
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Post } from '../model/Ipost.model';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -14,14 +15,26 @@ export class PostCreateComponent implements OnInit {
   newPost = '';
   enteredTitle = '';
   enteredContent = '';
+  form: FormGroup;
   post: Post;
+  imagePreview: string;
+  isLoading = false;
   private mode = 'create';
   private postId: string;
-  isLoading = false;
+
 
   constructor(private postService: PostsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null,
+        {validators: [Validators.required, Validators.minLength(3)]}
+      ),
+      content: new FormControl(null,
+        {validators: [Validators.required, Validators.minLength(3)]}
+      ),
+      image: new FormControl(null, {validators: [Validators.required], asyncValidators:[mimeType]})
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if(paramMap.has('postId')) {
         this.mode = 'edit';
@@ -30,24 +43,45 @@ export class PostCreateComponent implements OnInit {
         this.postService.getPost(this.postId).subscribe(postData =>{
           this.isLoading = false;
           this.post = {id:postData._id, title:postData.title, content: postData.content};
+          this.form.setValue({
+            title: this.post.title,
+            content:this.post.content
+          });
         });
+
       } else {
         this.mode = 'create';
         this.postId = null;
       }
     });
   }
-  onSavePost(postForm:NgForm): void {
+  onSavePost(postForm:any): void {
     if (postForm.invalid) {
       return;
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postService.addPost(postForm.value.title, postForm.value.content);
+      this.postService.addPost(postForm.title, postForm.content);
     } else {
-      this.postService.updatePost(this.postId, postForm.value.title, postForm.value.content);
+      this.postService.updatePost(this.postId, postForm.title, postForm.content);
     }
-    postForm.resetForm();
+    this.form.reset();
+  }
+
+  onImagePicked(event: Event) {
+    // Telling typescript that this will be HTMLInputElement
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({
+      image: file
+    });
+    // mmmm I am pretty sure you don't have to call updateValueAndValidity after a patch.
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    // set to function because async will take awhile so set to function and callback
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
 }
