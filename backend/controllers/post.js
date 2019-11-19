06721467fe10be265455c5396088114252158606
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2019-11-18 22:10:44 -0700

    bug fix on update

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -75,8 +75,9 @@ exports.updatePost = (req,res,next) => {
     imagePath: imagePath,
     creator: req.userData.userId
   });
+
   Post.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result => {
-    if(result.nModified > 0){
+    if(result.n > 0){
       res.status(200).json({message: "Update successful"});
     }else{
       res.status(401).json({
